298503dada4c8ef3abf9e94dec3d35e86e3fd669
Claus Färber <CFAERBER@cpan.org>
2007-10-12 15:25:28 +0000

    Encode needs perl v5.7.3, make it a separate dist again.
    git-svn-id: https://net-idn-encode.svn.sourceforge.net/svnroot/net-idn-encode/trunk/Encode-Punycode@72 9baecffb-6522-0410-992d-acb0baa9b834

--- lib/Encode/Punycode.pm
+++ lib/Encode/Punycode.pm
@@ -1,38 +1,53 @@
+# $Id : $
+
 package Encode::Punycode;
 
 use strict;
-our $VERSION = 0.02;
+use utf8;
+require 5.006_000;
+
+our $VERSION = '0.99_20071012';
+$VERSION = eval $VERSION;
 
 require Encode;
 use base qw(Encode::Encoding);
 __PACKAGE__->Define('Punycode', 'punycode');
 
-require IDNA::Punycode;
+use Net::IDN::Punycode();
 
 sub encode {
-    my($obj, $str, $chk) = @_;
-    $str = IDNA::Punycode::encode_punycode($str);
-    $_[1] = '' if $chk;
-    return $str;
+  my($self, $string, $check) = @_;
+  $string = Net::IDN::Punycode::encode_punycode($string);
+  $_[1] = '' if $check;
+  return $string;
 }
 
 sub decode {
-    my($obj, $str, $chk) = @_;
-    $str = IDNA::Punycode::decode_punycode($str);
-    $_[1] = '' if $chk;
-    return $str;
+  my($self, $string, $check) = @_;
+  $string = Net::IDN::Punycode::decode_punycode($string);
+  $_[1] = '' if $check;
+  return $string;
+}
+
+sub mime_name {
+  return undef;
+};
+
+sub perlio_ok { 
+  return 0;
 }
 
 1;
 __END__
 
+=encoding utf8
+
 =head1 NAME
 
-Encode::Punycode - Encode plugin for Punycode encodings
+Encode::Punycode - Encode plugin for Punycode (S<RFC 3492>)
 
 =head1 SYNOPSIS
 
-  use Encode::Punycode;
   use Encode;
 
   $unicode  = decode('Punycode', $punycode);
@@ -40,21 +55,29 @@ Encode::Punycode - Encode plugin for Punycode encodings
 
 =head1 DESCRIPTION
 
-Encode::Punycode is an Encode plugin, which allows you to encode
-Unicode strings into Punycode. Punycode is an efficient encoding
-(ACE) of Unicode for use with IDNA.
+Encode::Punycode is an Encode plugin, which implements the
+Punycode encoding.  Punycode is an instance of a more general
+algorithm called Bootstring, which allows strings composed from a
+small set of "basic" code points to uniquely represent any string
+of code points drawn from a larger set.  Punycode is Bootstring
+with particular parameter values appropriate for IDNA.
+
+Note that this module does not do any string preparation as
+specified by I<nameprep>/I<stringprep>. It does not do add any
+prefix or suffix, either.
 
 =head1 AUTHOR
 
-Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+Claus Färber E<lt>CFAERBER@cpan.orgE<gt>
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+Previous versions written by Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-http://www.ietf.org/internet-drafts/draft-ietf-idn-punycode-01.txt
-
-L<IDNA::Punycode>
+L<Encode>, L<Net::IDN::Punycode>, 
+S<RFC 3492> L<http://www.ietf.org/rfc/rfc3492.txt>
 
 =cut

ee6e1d8a4ceac09883a1d58a13037ec4317d1d27
Claus Färber <cfaerber@cfaerber.name>
2013-07-27 22:37:00 +0200

    extra tests for non-XS version on XS-capable systems

--- lib/Net/IDN/Punycode.pm
+++ lib/Net/IDN/Punycode.pm
@@ -16,8 +16,10 @@ our @EXPORT = ();
 our @EXPORT_OK = ();
 our %EXPORT_TAGS = ( 'all'  => [ qw(encode_punycode decode_punycode) ], );
 Exporter::export_ok_tags(keys %EXPORT_TAGS);
+our $_NO_XS = undef;
 
 eval { 
+  die if $_NO_XS;
   require XSLoader;
   XSLoader::load('Net::IDN::Punycode'); 
 };
